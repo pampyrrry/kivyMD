f084a9ffab8418f4bbe4e4f109fbe4d6a147648e
Daniel Mielcarek <mielcarek2012@gmail.com>
2023-12-16 22:34:10 +0100

    Aler dialog

--- yt-course/48_alert_dialog.py
+++ yt-course/48_alert_dialog.py
@@ -1,25 +1,42 @@
+#https://www.youtube.com/watch?v=tToJBfDgCsc&list=PLCC34OHNcOtpz7PJQ7Tv7hqFBP_xDDjqg&index=48
 
 from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
 
 
 class MainApp(MDApp):
+    dialog = None
 
-    data = {
-        #    Text           Icon
-        "Python": "language-python",
-        "Ruby": "language-ruby",
-        "JS": "language-javascript"
-    }
 
-    def callback(self, instance):
-        self.root.ids.my_label.text = {instance.icon}
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title = "Pretty Neat, Right?",
+                text = "This is just a text",
+                buttons = [
+                    MDFlatButton(
+                        text="Cancel",
+                        text_color= self.theme_cls.primary_color,
+                        on_release = self.close_dialog),
+                    MDRectangleFlatButton(
+                        text = "Yes It's Neat!",
+                        text_color= self.theme_cls.primary_color,
+                        on_release= self.neat_dialog)
+                ]
+            )
+        self.dialog.open()
 
-    def open(self):
-        self.root.ids.my_label.text = "Open!"
+    #Click canel button
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
 
-    def close(self):
-        self.root.ids.my_label.text = "Close!"
+    #Click neat button
+    def neat_dialog(self, obj):
+        self.dialog.dismiss()
+        #Change label text
+        self.root.ids.my_label.text = "Yes It's Neat"
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
